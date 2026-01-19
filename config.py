a7dd16302527722520d4ac11d7e03c93bdb86a65
mrrmj <mrrj23046@gmail.com>
2026-01-19 17:54:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "8222374273:AAGXnsmGkU2-rBBPDNW7qRMfpX9IDMOw-4Y")
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "8384647509:AAG9yVwaQWm2fG8aje1RGTkOPnHalM0C_lE")
 WINGO_API_URL = "https://draw.ar-lottery01.com/WinGo/WinGo_30S/GetHistoryIssuePage.json"
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://user:password@host:port/database")
 
